96b7ec124075199badab9cdd7d6a6266bf1c1ab3
Nathan L <aaltol@HUAWEI_Mate_20-b2e69b6354.localdomain>
2021-12-06 11:38:18 +0800

    Add main & test.py

--- main_test.py
+++ main_test.py
@@ -1,16 +1,17 @@
 import unittest
-from main import print_hi_stephen, print_hi_you#, print_hi_?
+from main import print_hi_stephen, print_hi_you, print_hi_jerry  # , print_hi_?
 
 
 class MyTestCase(unittest.TestCase):
     def test_print_hi_stephen(self):
         self.assertEqual(print_hi_stephen(), f"Hi, Stephen")
 
-    def test_print_hi_you(self):
-        self.assertEqual(print_hi_you(), f"")
 
-    #def test_print_hi_?(self):
-        #
+    def test_print_hi_you(self):
+        self.assertEqual(print_hi_you(), f"Hi, you")
+
+    def test_print_hi_(self):
+        self.assertEqual(print_hi_jerry(), f"Hi, jerry")
 
 
 if __name__ == '__main__':
